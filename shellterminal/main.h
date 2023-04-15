9470fdd0c5d428c06c58ddab7a7082a5194d5508
devluanga <imaniimaniluanga@gmail.com>
2023-04-15 16:55:21 +0300

    update

---
+++ shellterminal/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+// #include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+#endif
